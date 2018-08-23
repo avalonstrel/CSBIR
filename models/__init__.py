d61c268efce3ed8a8fdf915788b1f763beaabde5
Peng Lu <penglu2097@gmail.com>
2018-08-23 17:36:45 +0800

    update

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,6 @@
 from .losses import SphereLoss, CentreLoss, AttributeLoss, ClassificationLoss
 from .networks import DenseNet169, Resnet50, Resnet50, Resnext100, Pnasnet5
 losses = {'sphere':SphereLoss, 'centre':CentreLoss, 'attribute':AttributeLoss, 'softmax':ClassificationLoss}
-networks = {'resnet50':Resnet50, 'densenet169':DenseNet169, 'Resnet50':Resnet50, 'Resnet100':Resnext100, 'Pnasnet5':Pnasnet5}
+networks = {'resnet50':Resnet50, 'densenet169':DenseNet169, 'resnet50':Resnet50, 'resnet100':Resnext100, 'pnasnet5':Pnasnet5}
 
 __all__ = [networks, losses]
