f63edfa2a00d250c41c30024f3da4e3084447c23
Peng Lu <penglu2097@gmail.com>
2018-08-23 16:44:10 +0800

    network

--- models/networks.py
+++ models/networks.py
@@ -6,6 +6,8 @@ import torch.nn.functional as F
 from .densenet import densenet169
 from .resnet import resnet50
 from .model_utils import *
+import pretrainedmodels
+
 
 class DenseNet169(torch.nn.Module):
     def __init__(self, f_dim=512, pretrained=True, **kwargs):
@@ -45,3 +47,62 @@ class Resnet50(nn.Module):
         return feat        
 
 
+class Resnext50(nn.Module):
+    def __init__(self, f_dim, pretrained=True, **kwargs):
+        super(Resnext50, self).__init__()
+
+        model_name = 'se_resnext50_32x4d' # could be fbresnet152 or inceptionresnetv2
+        self.model = pretrainedmodels.__dict__[model_name](num_classes=1000, pretrained='imagenet').features
+        self.pool = nn.AvgPool2d(kernel_size=7, stride=1, padding=0)
+        self.fc = nn.Linear(2048, f_dim, bias=True)
+
+        self.norm = kwargs.get('norm', False)
+
+    def forward(self, x, mode_flag='pho'):
+        feat = self.features(x)
+        feat = self.pool(feat).squeeze(3).squeeze(2)
+        feat = self.fc(feat)
+
+        if self.norm:
+            feat = F.normalize(feat, p=2, dim=1)
+        return feat          
+
+class Resnext100(nn.Module):
+    def __init__(self, f_dim, pretrained=True, **kwargs):
+        super(Resnext100, self).__init__()
+
+        model_name = 'se_resnext101_32x4d' # could be fbresnet152 or inceptionresnetv2
+        self.model = pretrainedmodels.__dict__[model_name](num_classes=1000, pretrained='imagenet').features
+        self.pool = nn.AvgPool2d(kernel_size=7, stride=1, padding=0)
+        self.fc = nn.Linear(2048, f_dim, bias=True)
+
+        self.norm = kwargs.get('norm', False)
+
+    def forward(self, x, mode_flag='pho'):
+        feat = self.features(x)
+        feat = self.pool(feat).squeeze(3).squeeze(2)
+        feat = self.fc(feat)
+
+        if self.norm:
+            feat = F.normalize(feat, p=2, dim=1)
+        return feat          
+
+class Pnasnet5(nn.Module):
+    def __init__(self, f_dim, pretrained=True, **kwargs):
+        super(Pnasnet5, self).__init__()
+
+        model_name = 'pnasnet5large' # could be fbresnet152 or inceptionresnetv2
+        self.model = pretrainedmodels.__dict__[model_name](num_classes=1000, pretrained='imagenet').features
+        self.pool = nn.AvgPool2d(kernel_size=7, stride=1, padding=0)
+        self.fc = nn.Linear(4320, f_dim, bias=True)
+
+        self.norm = kwargs.get('norm', False)
+
+    def forward(self, x, mode_flag='pho'):
+        feat = self.features(x)
+        feat = self.pool(feat).squeeze(3).squeeze(2)
+        feat = self.fc(feat)
+
+        if self.norm:
+            feat = F.normalize(feat, p=2, dim=1)
+        return feat          
